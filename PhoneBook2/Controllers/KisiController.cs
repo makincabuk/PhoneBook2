d651cbcf25fe75f4cd74d2f547fdbfe3eead75f4
mehmet akın çabuk <makincabuk@gmail.com>
2023-02-18 06:30:30 +0300

    V3 -Html helper ile ekle sayfamızı tasarladık. -Ekle sayfamızdan gelen formdaki kişiler verisini veritabanına eklemek için KisiController yapısı oluşturuldu. HttpPost yapısı ile Formdan gelen Kisi verisine Guid oluşturup verilerimizi veritabanına işledik.

---
+++ PhoneBook2/Controllers/KisiController.cs
@@ -0,0 +1,37 @@
+﻿using PhoneBook2.Models.Context;
+using PhoneBook2.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Diagnostics;
+
+namespace PhoneBook2.Controllers
+{
+    public class KisiController : Controller
+    {
+        PhoneContext db = new PhoneContext();
+
+        // GET: Kisi
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Ekle()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Ekle(Kisi xkisi)
+        {
+            Guid xUuid = Guid.NewGuid();
+            xkisi.UUID = xUuid.ToString();
+
+            db.Kisiler.Add(xkisi);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
